08c6963f44d442a18386868103e0ec4c34fbad2a
Anton Zhilinskey <zhilinskiianton@gmail.com>
2025-01-24 12:29:48 +0700

    Added alembic

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/test_db"
+DATABASE_URL = "postgresql+asyncpg://user:password@db/test_db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
